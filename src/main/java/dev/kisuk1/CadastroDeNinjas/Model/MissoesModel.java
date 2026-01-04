cb2e2d69710aabc203f824d70dea2fc9b66a9f1b
PauloRe1s <paullogdx01@gmail.com>
2026-01-04 10:24:23 -0300

    feat: Cria Entity de Ninjas e Missões na Model com Relacionamento entre as mesmas

--- src/main/java/dev/kisuk1/CadastroDeNinjas/Model/MissoesModel.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Model/MissoesModel.java
@@ -1,4 +1,30 @@
 package dev.kisuk1.CadastroDeNinjas.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_missoes")
+    private Long id;
+
+    @Column(name = "nome",nullable = false)
+    private String nome;
+
+    @Column(name = "dificuldade",nullable = false)
+    private String dificuldade;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjasModel> ninja;
 }
--- src/main/java/dev/kisuk1/CadastroDeNinjas/Model/NinjasModel.java
+++ src/main/java/dev/kisuk1/CadastroDeNinjas/Model/NinjasModel.java
@@ -1,4 +1,37 @@
 package dev.kisuk1.CadastroDeNinjas.Model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_ninjas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_ninjas")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "idade",nullable = false)
+    private int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id")
+    private List<MissoesModel> missoes;
 }
